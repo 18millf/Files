e1cab8de8a925c9061d2969d3ed9079efe5feab0
Freddie Mills <18millf@ola.org.uk>
2023-11-10 15:26:20 +0000

    namesake

---
+++ full_geography_quiz.py
@@ -0,0 +1,32 @@
+from io import TextIOWrapper
+from random import randrange
+
+QUESTION_COUNT = 10
+
+# Give user 10 questions.
+# At the end give a % mark.
+
+file: TextIOWrapper = open("resources/countriescapitals.txt")
+lines: list[str] = file.readlines()
+
+score: int = 0
+
+for i in range (0, 10):
+    random_line: int = randrange(0, len(lines));
+    selection: str = lines[random_line]
+    country_capital_pair: list[str] = selection.strip().split(",")
+
+    print(f"Question {i + 1}/{QUESTION_COUNT}:")
+    response: str = input(f"What is the capital of {country_capital_pair[0]}? ").lower()
+
+    answer: str = country_capital_pair[1].lower()
+
+    if response == answer:
+        print("Correct!")
+        score += 1
+    else:
+        print(f"Incorrect. The correct answer was {answer}.")
+    print()
+
+percentage: float = (score / QUESTION_COUNT) * 100
+print(f"Finished! Your score was {score}/{QUESTION_COUNT} ({percentage:.1f}%)")
